06415e4830952a56335da6a8cad55fdfcb16e0b1
root <1730495747@qq.com>
2023-11-05 13:19:35 +0800

    api对象

--- flask_server/user/__init__.py
+++ flask_server/user/__init__.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
-
+from flask_restful import Api
 
 # 创建蓝图对象
 user_bp = Blueprint('user', __name__,url_prefix='/user')
-
+# 创建api对象
+user_api = Api(user_bp)
 
 # 导入视图
 from . import views
