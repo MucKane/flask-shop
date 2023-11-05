f84c4ce093bb8c32aa6265cec9c2645e21452097
root <1730495747@qq.com>
2023-11-05 13:45:49 +0800

    add register logic

--- flask_server/user/views.py
+++ flask_server/user/views.py
@@ -1,6 +1,8 @@
-from flask_server.user import user_bp
-from flask_server.models import User
+from flask_server.user import user_bp,user_api
+from flask_server import models 
 from flask import request
+from flask_restful import Resource
+import re
 # 创建视图
 @user_bp.route('/')
 def index():
@@ -19,10 +21,52 @@ def login():
     return {'status': 400, 'msg': '参数不完整'}
   else:
     # 通过用户名获取用户对象
-    user = User.query.filter(name == name).first()
+    user = models.User.query.filter(name == name).first()
     # 判断用户是否存在
     if user:
       # 判断密码是否正确
       if user.check_password(password):
         return {'status': 200, 'msg': '登录成功'}
     return {'status': 400, 'msg': '用户名或密码错误'}
+
+# 注册功能
+class Users(Resource):
+  def get(self):
+    pass
+  def post(self):
+    # 注册
+    # 接受用户信息
+    name = request.get_json().get('name')
+    pwd = request.get_json().get('pwd')
+    rel_password = request.get_json().get('rel_password')
+    nick_name = request.get_json().get('nick_name')
+    # 验证数据的合法性
+    if not all([name, pwd, rel_password]):
+      return {'status': 400, 'msg': '参数不完整'}
+    # 判断两次密码是否一致
+    if pwd != rel_password:
+      return {'status': 400, 'msg': '两次密码不一致'}
+    # 接受手机号与邮箱
+    phone = request.get_json().get('phone')
+    email = request.get_json().get('email')
+    # 验证手机号格式
+    if not re.match(r'^1[3456789]\d{9}$',phone):
+      return {'status': 400, 'msg': '手机号格式不正确'}
+    # 验证邮箱格式
+    if not re.match(r'^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$',email):
+      return {'status': 400, 'msg': '邮箱格式不正确'}
+  
+    # 判断用户名是否存在
+    try:
+      user = models.User.query.filter(name == name).first()
+      if user:
+        return {'status': 400, 'msg': '用户名已存在'}
+    except Exception as e:
+      # 没有已经存在的用户 逻辑继续进行
+      pass
+    
+    # 创建对象
+    user = models.User(name=name, password=pwd, phone=phone, email=email,nick_name=nick_name)
+    return{'status': 200, 'msg': '注册成功'}
+
+user_api.add_resource(Users,'/users/')
